f2ce0b3221aa1036f238a9499d7908dde4b61a21
Dmitry Shmidt <dimitrysh@google.com>
2011-02-14 16:58:48 -0800

    net: wireless: Add get_country_code functionality to platform
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- include/linux/wlan_plat.h
+++ include/linux/wlan_plat.h
@@ -20,6 +20,7 @@ struct wifi_platform_data {
 	int (*set_reset)(int val);
 	int (*set_carddetect)(int val);
 	void *(*mem_prealloc)(int section, unsigned long size);
+	void *(*get_country_code)(char *ccode);
 };
 
 #endif
