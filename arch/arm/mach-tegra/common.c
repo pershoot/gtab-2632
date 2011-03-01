967ed500fe6c661838b40935dee813ed293a5b98
Prashant Somashekar <prashsomash@gmail.com>
2011-03-01 00:14:38 -0500

    tegra: common: change 0x7C080001 to 0x7C480001 in tegra_init_cache
    as per:
    http://android.git.kernel.org/?p=kernel/tegra.git;a=commitdiff;h=6c77fdda35688ae3c9d9029a9824f7cf941f348d
    
    Author of change:
    Chris Fries <C.Fries@motorola.com>
    
    In HEAD:
    android-tegra-2.6.36

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -98,7 +98,7 @@ void __init tegra_init_cache(void)
 	writel(0x441, p + L2X0_DATA_LATENCY_CTRL);
 	writel(7, p + L2X0_PREFETCH_OFFSET);
 
-	l2x0_init(p, 0x7C080001, 0x8200c3fe);
+	l2x0_init(p, 0x7C480001, 0x8200c3fe);
 #endif
 }
 
