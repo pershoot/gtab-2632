a8f5f5626db2e6ce0a47de28427f272c615c90e7
Prashant Somashekar <prashsomash@gmail.com>
2011-04-15 18:44:21 -0400

    common: clocks: increase the low end voltage to 850.

--- arch/arm/mach-tegra/nvrm/core/common/nvrm_clocks_limits.c
+++ arch/arm/mach-tegra/nvrm/core/common/nvrm_clocks_limits.c
@@ -61,10 +61,10 @@
 // Voltage list for corresponding clocks
 NvU32 FakeShmooVoltages[] = {
 #if defined(CONFIG_ZPAD_T2_NEWBL)
-	825,
+	850,
 #endif
-	825,
-	825,
+	850,
+	850,
 	875,
 	925,
 	1000,
