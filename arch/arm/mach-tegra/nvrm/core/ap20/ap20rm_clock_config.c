a9365898157c6b5fa9ad17f56a4f907df7b14eac
Prashant Somashekar <prashsomash@gmail.com>
2011-04-17 23:26:00 -0400

    core: ap15/20 clocks: Force CPU clock to 1GHZ on initialisation, if using FAKE_SHMOO.

--- arch/arm/mach-tegra/nvrm/core/ap20/ap20rm_clock_config.c
+++ arch/arm/mach-tegra/nvrm/core/ap20/ap20rm_clock_config.c
@@ -57,6 +57,8 @@
 // Minimum core over CPU voltage margin (at SoC)
 #define NV_AP20_CORE_OVER_CPU_MV (120)
 
+#define USE_FAKE_SHMOO
+
 /*****************************************************************************/
 
 /*
@@ -1752,7 +1754,11 @@ NvRmPrivAp20FastClockConfig(NvRmDeviceHandle hRmDevice)
 
     // Set PLLX0 and CPU clock to SoC maximum (cpu voltage is guaranteed to
     // be nominal during initialization and resume as well)
+#ifdef USE_FAKE_SHMOO
+    CpuKHz = 1000000;
+#else
     CpuKHz = NvRmPrivGetSocClockLimits(NvRmModuleID_Cpu)->MaxKHz;
+#endif
     FreqKHz = NvRmPrivGetClockSourceFreq(NvRmClockSource_PllX0);
     if (CpuKHz != FreqKHz)
     {
